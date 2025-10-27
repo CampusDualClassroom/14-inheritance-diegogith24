e972d4c54cfe9dcd0b144f5cb9208f8bb2843ab7
Diego Armando Castillo Montoya <castillodiego2401@gmail.com>
2025-10-27 18:43:37 +0100

    Solved Exercise 14

--- src/main/java/com/campusdual/classroom/Exercise.java
+++ src/main/java/com/campusdual/classroom/Exercise.java
@@ -6,5 +6,14 @@ public class Exercise {
         Teacher t = new Teacher("Maria", "Montessori", "Educación");
         PoliceOfficer po = new PoliceOfficer("Jake", "Peralta", "B-99");
         Doctor d = new Doctor("Gregory", "House", "Nefrología e Infectología");
+        System.out.println("###### Teacher ######");
+        t.getDetails();
+        System.out.println("###### Police Officer ######");
+        po.getDetails();
+        System.out.println("###### Doctor ######");
+        d.getDetails();
+
+
+
     }
 }
